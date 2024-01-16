6ad5b0fc494029c4005245f055204250fec5b115
Weiter4568 <90168326+Weiter4568@users.noreply.github.com>
2024-01-16 14:18:36 +0800

    Update email.py

--- push/email.py
+++ push/email.py
@@ -1,20 +1,22 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
-def push(title,htmlcontent,config):
-    sender=config['sender']
-    passwd=config['password']
-    receiver=[config['receiver']]
+
+
+def push(title, htmlcontent, config):
+    sender = config['sender']
+    passwd = config['password']
+    receiver = config['receiver']
     message = MIMEText(htmlcontent, 'html', 'utf-8')
     message['From'] = sender
     message['To'] = receiver
     message['Subject'] = Header(title, 'utf-8')
     try:
-        smtp = smtplib.SMTP()
-        smtp.connect(config['host'],config['port'])
+        smtp = smtplib.SMTP('smtp.qq.com',587)  # 使用SMTP_SSL创建一个SSL/TLS加密的连接
+        smtp.starttls()
         smtp.login(sender, passwd)
         smtp.sendmail(sender, receiver, message.as_string())
         print("email推送成功！")
         smtp.quit()
-    except smtplib.SMTPException:
-        print('email推送失败')
+    except Exception as e:
+        print('email推送失败:', e)
